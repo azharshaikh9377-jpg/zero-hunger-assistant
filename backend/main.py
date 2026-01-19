0546e9c1cb93c7bd1c76e5fed4b8b6ad9bb166e3
azharshaikh9377-jpg <azharshaikh93.77@gmail.com>
2026-01-19 21:18:33 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -70,5 +70,16 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
 
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 
 
